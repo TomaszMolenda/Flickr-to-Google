c0a61383399433e3d822c3075f99cf7d24e968e1
tomasz.molenda <tomasz.molenda@gmail.com>
2017-10-29 18:04:39 +0100

    add scheduler

--- src/main/java/pl/tomo/flickrtogoogle/FlickrtogoogleApplication.java
+++ src/main/java/pl/tomo/flickrtogoogle/FlickrtogoogleApplication.java
@@ -2,8 +2,10 @@ package pl.tomo.flickrtogoogle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class FlickrtogoogleApplication {
 
 	public static void main(String[] args) {
