4f76e527271e7f1238a10039f1b9a3e75af584d2
tomasz.molenda <tomasz.molenda@gmail.com>
2017-10-29 17:36:48 +0100

    init commit

---
+++ src/main/java/pl/tomo/flickrtogoogle/FlickrtogoogleApplication.java
@@ -0,0 +1,12 @@
+package pl.tomo.flickrtogoogle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlickrtogoogleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlickrtogoogleApplication.class, args);
+	}
+}
