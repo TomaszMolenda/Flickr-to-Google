d594de6b5f9036888a452d240683b7155a3ba4a5
tomasz.molenda <tomasz.molenda@gmail.com>
2017-11-30 18:10:26 +0100

    change exetutable to jar and set sa password to access by www

--- src/main/java/pl/tomo/flickrtogoogle/FlickrtogoogleApplication.java
+++ src/main/java/pl/tomo/flickrtogoogle/FlickrtogoogleApplication.java
@@ -7,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-//@EnableBatchProcessing
 @EnableAsync
 public class FlickrtogoogleApplication {
 
